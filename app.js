18db2d70651f58c72e8950ee22eb42e656dfd87e
somkene <somkenemuscle@gmail.com>
2022-09-16 03:00:50 +0100

    rockpapersci

---
+++ app.js
@@ -0,0 +1,77 @@
+let game = ["ROCK", "PAPER", "SCISSORS"]
+// let rock = document.getElementById("rock")
+// let paper = document.getElementById("paper")
+// let scissors = document.getElementById("scissors")
+let btn = document.querySelectorAll(".btn input")
+let situation = document.getElementById("status")
+let pscore = document.getElementById("pscore")
+let cscore = document.getElementById("cscore")
+
+let p1score = 0;
+let c1score = 0;
+
+
+
+btn.forEach(bt => {
+    bt.addEventListener('click', () => {
+       let random = Math.floor(Math.random() * game.length);
+
+       //ROCK
+      if (bt.value === game[random]) {
+        situation.innerHTML = "The Bot Played " + game[random] + " ,You Chose " + bt.value +  " ,It's A tie :|"
+      }else if (bt.value === "ROCK" && game[random] === "PAPER") {
+        situation.innerHTML = "The Bot Played " + game[random] + " ,You Chose " + bt.value +  " ,Computer Wins :("
+        c1score+=1
+        cscore.innerHTML = c1score;
+      }else if (bt.value === "PAPER" && game[random] === "ROCK") {
+        situation.innerHTML = "The Bot Played " + game[random] + " ,You Chose " + bt.value +  " ,So You Win :)"
+        p1score+=1
+        pscore.innerHTML = p1score;
+      }else if (bt.value === "ROCK" && game[random] === "SCISSORS") {
+        situation.innerHTML = "The Bot Played " + game[random] + " ,You Chose " + bt.value +  " ,So You Win :)"
+        p1score+=1
+        pscore.innerHTML = p1score;
+      }else if (bt.value === "SCISSORS" && game[random] === "ROCK") {
+        situation.innerHTML = "The Bot Played " + game[random] + " ,You Chose " + bt.value +  " ,Computer WinS :("
+        c1score+=1
+        cscore.innerHTML = c1score;
+      }
+      //PAPER
+      else if (bt.value === "PAPER" && game[random] === "ROCK") {
+        situation.innerHTML = "The Bot Played " + game[random] + " ,You Chose " + bt.value +  " ,So You Win :)"
+        p1score+=1
+        pscore.innerHTML = p1score;
+      }else if (bt.value === "ROCK" && game[random] === "PAPER") {
+        situation.innerHTML = "The Bot Played " + game[random] + " ,You Chose " + bt.value +  " ,Computer WinS :("
+        c1score+=1
+        cscore.innerHTML = c1score;
+      }else if (bt.value === "PAPER" && game[random] === "SCISSORS") {
+        situation.innerHTML = "The Bot Played " + game[random] + " ,You Chose " + bt.value +  " ,Computer Wins :("
+        c1score+=1
+        cscore.innerHTML = c1score;
+      }else if (bt.value === "SCISSORS" && game[random] === "PAPER") {
+        situation.innerHTML = "The Bot Played " + game[random] + " ,You Chose " + bt.value +  " ,So You Win :)"
+        p1score+=1
+        pscore.innerHTML = p1score;
+      }
+      //SCISSORS
+      else if (bt.value === "SCISSORS" && game[random] === "ROCK") {
+        situation.innerHTML = "The Bot Played " + game[random] + " ,You Chose " + bt.value +  " ,Computer Wins :("
+        c1score+=1
+        cscore.innerHTML = c1score;
+      }else if (bt.value === "ROCK" && game[random] === "SCISSORS") {
+        situation.innerHTML = "The Bot Played " + game[random] + " ,You Chose " + bt.value +  " ,So You Win :)"
+        p1score+=1
+        pscore.innerHTML = p1score;
+      }else if (bt.value === "PAPER" && game[random] === "SCISSORS") {
+        situation.innerHTML = "The Bot Played " + game[random] + " ,You Chose " + bt.value +  " ,Computer Wins :("
+        c1score+=1
+        cscore.innerHTML = c1score;
+      }else if (bt.value === "SCISSORS" && game[random] === "PAPER") {
+        situation.innerHTML = "The Bot Played " + game[random] + " ,You Chose " + bt.value +  " ,So You Win :)"
+        p1score+=1
+        pscore.innerHTML = p1score;
+      }
+
+    });
+});
